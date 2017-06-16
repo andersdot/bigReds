070a7201d179277137746e85b9a3a43db8a33cb4
andersdot <landerson@simonsfoundation.org>
2017-06-16 16:28:57 -0400

    getting spectra

--- separation.py
+++ separation.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import figsizemargins
 import matplotlib.patches as patches
 import astropy.io.fits as io
+import glob
 
 def plotSpec(wave, spec, fit, err):
     ratio = fit/spec
@@ -26,32 +27,41 @@ def plotSpec(wave, spec, fit, err):
     plt.close(fig)
 
 
-def getSpectra(objID, wave=True):
+def getSpectra(objID, wave=True, nPixel=8575):
     dataDir = '/mnt/ceph/users/landerson/l30e.2/'
     filename = 'aspcapStar-r6-l30e.2-' + objID + '.fits'
-    out = grep.grep(dataDir + '*/' + filename)[0]
-    hdu = fits.open(out)
-    spec = hdu[1].data
-    err = hdu[2].data
-    fit = hdu[3].data
-    nPixel = len(spec)
-    if wave:
-         wave = 10.**(hdu[1].header['CRVAL1'] + np.arange(nPixel)*hdu[1].header['CDELT1'])
-         return spec, err, fit, wave
-    else:
-        return spec, err, fit, wave
+    out = glob.glob(dataDir + '*/' + filename)
+    try:
+        filen =  out[0]
+        hdu = fits.open(filen)
+        spec = hdu[1].data
+        err = hdu[2].data
+        fit = hdu[3].data
+        nPixel = len(spec)
+        if wave:
+            wave = 10.**(hdu[1].header['CRVAL1'] + np.arange(nPixel)*hdu[1].header['CDELT1'])
+            return spec, err, fit, wave
+        else:
+            return spec, err, fit
+    except IndexError:
+        print filename, out
+        foo = np.zeros(nPixel)
+        foo.fill(np.nan) 
+        return foo, foo, foo
 
 plot=False
 data = fits.getdata('allStar-l30e.2.fits')
 
 good = data['LOGG'] != -9999.0
 
+#define inital box
 Lmin = 2
 Lmax = 3.2
 Tmin = 4200
 Tmax = 5500
 rc = (data['TEFF'] > Tmin) & (data['TEFF'] < Tmax) & (data['LOGG'] > Lmin) & (data['LOGG'] < Lmax)
 
+#define zoom of inital box
 LminZ = 2.38
 LmaxZ = 2.42
 TminZ = 4650
@@ -75,18 +85,26 @@ if plot:
     fig.savefig('apogeeTeffLogg.pdf', dpi=400)
     plt.close(fig)
 
+try: 
+    specMatrix = np.load('specMatrix.npy')
+    print 'read in npy matrix'
 
-nstars = np.sum(rcZoom)
-nPixels = 8575
-specMatrix = np.zeros((nstars, nPixels))
+except IOError:
 
-for i, index in enumerate(np.where(rcZoom)[0])
-    objID = data['APSTAR_ID'][index].split('.')[-1]
-    spec, err, fit = getSpectra(objID, wave=False)
-    specMatrix[i] = spec
-    if plot:
-        plotSpec(wave, spec, fit, err)
+    nstars = np.sum(rcZoom)
+    nPixels = 8575
+    specMatrix = np.zeros((nstars, nPixels))
 
-U ,s, V = numpy.linalg.svd(specMatrix)
+    for i, index in enumerate(np.where(rcZoom)[0]):
+        objID = data['APSTAR_ID'][index].split('.')[-1]
+        spec, err, fit = getSpectra(objID, wave=False)
+        specMatrix[i] = spec
+        if plot:
+            plotSpec(wave, spec, fit, err)
+
+    np.save('specMatrix', specMatrix)
+
+print 'calculating SVD'
+U ,s, V = np.linalg.svd(specMatrix)
 
 print 'length of rcZoom: ', np.sum(rcZoom)
